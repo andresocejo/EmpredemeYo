aed788559a6f01293d1f6e61bfc342b5f45277ea
Jhordan <erik_inho109@hotmail.com>
2014-08-04 18:54:10 -0500

    Skeleton-Update

---
+++ app/src/main/java/com/example/jhordan/semprende/main/Splash_Screen.java
@@ -0,0 +1,21 @@
+package com.example.jhordan.semprende.main;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.jhordan.semprende.R;
+
+/**
+ * Created by Jhordan on 04/08/14.
+ */
+public class Splash_Screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+    }
+}
