c5741295d458858df905c59f06d3a4bcf4c52a2d
DeepSil727 <petterlx727@gmail.com>
2014-08-08 19:47:18 -0500

    -Iconos Actualizados

--- app/src/main/java/com/example/jhordan/semprende/util/MiscellaneousMethods.java
+++ app/src/main/java/com/example/jhordan/semprende/util/MiscellaneousMethods.java
@@ -1,7 +1,5 @@
 package com.example.jhordan.semprende.util;
 
-import android.util.Log;
-
 import java.util.Comparator;
 
 /**
@@ -20,9 +18,7 @@ public class MiscellaneousMethods {
     public static class CustomComparator implements Comparator<Event> {
         @Override
         public int compare(Event ev1, Event ev2) {
-            Log.i("Substring 1 ",ev1.getTimeInit().substring(3,5));
-            Log.i("Substring 2 ",ev2.getTimeInit().substring(3,5));
-             return Integer.parseInt(ev1.getTimeInit().substring(0,2) + ev1.getTimeInit().substring(3,5)) - Integer.parseInt(ev2.getTimeInit().substring(0,2) + (ev2.getTimeInit().substring(3,5)));
+          return Integer.parseInt(ev1.getTimeInit().substring(0,2) + ev1.getTimeInit().substring(3,5)) - Integer.parseInt(ev2.getTimeInit().substring(0,2) + (ev2.getTimeInit().substring(3,5)));
         }
     }
 }
